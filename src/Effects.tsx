4dc73c119374f6c39806205868ec6f308c3584c7
vachagunn <vachikpuper@gmail.com>
2022-05-04 16:41:51 +0500

    Задание на рендеринг JSX

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [data, setData] = useState(-1);
+
+    useEffect(() => {
+        const callback = (data: number) => setData(data);
+        subscribe(props.sourceId, callback);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+            setData(-1);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {data}
+        </div>
+    );
 }
